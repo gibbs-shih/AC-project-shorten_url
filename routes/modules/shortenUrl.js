3d146e7f33ade5f211ea7209239d4b496bf2e8b7
gibbs-shih <shih.gibbs@gmail.com>
2023-05-19 01:54:52 +0800

    feat: create sample function for random 5 characters

--- routes/modules/shortenUrl.js
+++ routes/modules/shortenUrl.js
@@ -1,7 +1,22 @@
 const express = require('express')
 const router = express.Router()
 
+const lowerCaseLetters = 'abcdefghijklmnopqrstuvwxyz'
+const upperCaseLetters = lowerCaseLetters.toUpperCase()
+const numbers = '1234567890'
+const range = (lowerCaseLetters.split('').concat(upperCaseLetters.split(''))).concat([...numbers])
+
+// random pickup up 
+function sample (array) {
+  const index = Math.floor(Math.random() * array.length)
+  return array[index]
+}
+
 router.post('/', (req, res) => {
+  let short = ''
+  for (let i = 0; i < 5; i ++) {
+    short += sample(range)
+  }
   res.render('shorten')
 })
 

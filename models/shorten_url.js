2f22602edf1def9902393af32ce1accab69c6210
gibbs-shih <shih.gibbs@gmail.com>
2023-05-19 17:52:58 +0800

    feat: update shortenUrl modules about shorten function

--- models/shorten_url.js
+++ models/shorten_url.js
@@ -2,11 +2,11 @@ const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 const urlSchema = new Schema ({
   oldUrl: {
-    type: string,
+    type: String,
     required: true,
   },
   short: {
-    type: string,
+    type: String,
     required: true,
   }
 })
--- routes/modules/shortenUrl.js
+++ routes/modules/shortenUrl.js
@@ -14,12 +14,14 @@ function sample (array) {
 }
 
 router.post('/', (req, res) => {
+  const oldUrl = req.body.inputUrl
   let short = ''
   for (let i = 0; i < 5; i ++) {
     short += sample(range)
   }
-  
-  res.render('shorten')
+  return Url.create({oldUrl, short})
+    .then(res.render('shorten', {short, oldUrl}))
+    .catch(error => console.log(error))
 })
 
 module.exports = router

d9dc6bc02c98cc481a596b0a3269bc43f456c6b2
gibbs-shih <shih.gibbs@gmail.com>
2023-05-22 00:07:37 +0800

    feat: update shorten function: same url get same short url

--- routes/modules/shortenUrl.js
+++ routes/modules/shortenUrl.js
@@ -14,13 +14,23 @@ function sample (array) {
 }
 
 router.post('/', (req, res) => {
-  const oldUrl = req.body.inputUrl
-  let short = ''
-  for (let i = 0; i < 5; i ++) {
-    short += sample(range)
-  }
-  return Url.create({oldUrl, short})
-    .then(res.render('shorten', {short, oldUrl}))
+  const input = req.body.inputUrl
+  return Url.find({ oldUrl: input })
+    .then(url => {
+      // existing url > get same short url
+      if (url.length) {
+        res.render('shorten', { short: url[0].short, oldUrl: input })
+      } else {
+        // new url > get short url
+        let short = ''
+        for (let i = 0; i < 5; i ++) {
+          short += sample(range)
+        }
+        Url.create({oldUrl: input, short})
+          .then(() => res.render('shorten', { short, oldUrl: input }))
+          .catch(error => console.log(error))
+      }
+    })
     .catch(error => console.log(error))
 })
 
